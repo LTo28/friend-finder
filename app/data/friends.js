cb6d4f04b9faa6c4007a214677a209fb15ec0790
LTo28 <to.larry92@gmail.com>
2019-05-13 21:48:27 -0700

    server connection and json data

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+//JSON Data
+
+let friends = [
+  {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Steve Jobs",
+    "photo": "http://www.lloydcole.com/wp-content/uploads/2008/09/stevejobsbig.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Bill Gates",
+    "photo": "https://pbs.twimg.com/profile_images/988775660163252226/XpgonN0X.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Sundar Pichai",
+    "photo": "http://www.jaywatson.com/blog/wp-content/uploads/2016/09/Watson_160630_2887_M_HR.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+]
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { join } = require('path')
+const app = express()
+
+//app.use(express.static(join(__dirname, '/app/public')))
+
+app.get
+
+app.listen(3000)
+//app.listen(process.env.PORT || 3000)
